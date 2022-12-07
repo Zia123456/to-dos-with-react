98cc56e87ec3952fcf99747eadec429c89da4ffe
Zia123456 <m.ziabakhteyari12@gmail.com>
2022-12-07 07:38:40 -0800

    all features added

--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-dupe-class-members */
 import React from 'react';
 import TodosList from './ToDosList';
 import Header from './Header';
@@ -6,23 +7,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 class ToDoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: 'Setup development environment',
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: 'Develop website and add content',
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: 'Deploy to live server',
-        completed: false,
-      },
-    ],
+    todos: [],
   };
 
   handleChange = (id) => {
@@ -70,6 +55,29 @@ class ToDoContainer extends React.Component {
       }),
     });
   };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then((response) => response.json())
+      .then((data) => this.setState({ todos: data }));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos);
+      localStorage.setItem('todos', temp);
+    }
+  }
+
+  componentDidMount() {
+    const temp = localStorage.getItem('todos');
+    const loadedTodos = JSON.parse(temp);
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos,
+      });
+    }
+  }
 
   render() {
     return (
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -17,6 +17,10 @@ class TodoItem extends React.Component {
     }
   };
 
+  componentWillUnmount() {
+    console.log('cleaning up...');
+  }
+
   render() {
     let viewMode = {};
     let editMode = {};
