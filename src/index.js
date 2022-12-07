445769dcaf937edc0247e3e392ced8d9517d88c9
Zia123456 <m.ziabakhteyari12@gmail.com>
2022-12-07 04:10:31 -0800

    components created

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// component file
+import ToDoContainer from './components/ToDoContainer';
+
+// stylesheet
+import './App.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ToDoContainer />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
